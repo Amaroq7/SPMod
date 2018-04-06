68d37672fcc1db49546228feae3799304908fc5a
Karol Szuster <karolsz9898@gmail.com>
2018-04-06 19:05:56 +0200

    [skip ci] Unify passing args

--- travis/build.sh
+++ travis/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p build
 cd build
-python3.6 /usr/bin/meson .. . --buildtype=release --warnlevel 3 -D dynamic_link=true
+python3.6 /usr/bin/meson .. . --buildtype release --warnlevel 3 -D dynamic_link=true
 python3.6 /usr/bin/meson configure
 ninja -v
