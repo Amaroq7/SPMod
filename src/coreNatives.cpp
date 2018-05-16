5869a331d02e29d565e7a377e27bb6c57ce1416d
Karol Szuster <karolsz9898@gmail.com>
2018-05-17 01:35:32 +0200

    Add cvar natives

--- src/coreNatives.cpp
+++ src/coreNatives.cpp
@@ -301,6 +301,130 @@ static cell_t core_prepareFwdString(SourcePawn::IPluginContext *ctx,
     return id.has_value() ? *id : -1;
 }
 
+// native float cvarGetFloat(const char[] name)
+static cell_t core_cvarGetFloat(SourcePawn::IPluginContext *ctx,
+                                const cell_t *params)
+{
+    char *cvarName;
+    ctx->LocalToString(params[1], &cvarName);
+
+    return sp_ftoc(CVAR_GET_FLOAT(cvarName));
+}
+
+// native void cvarGetString(const char[] name, char[] value, int size)
+static cell_t core_cvarGetString(SourcePawn::IPluginContext *ctx,
+                                 const cell_t *params)
+{
+    char *cvarName, *destBuffer;
+    size_t bufferSize = params[3];
+
+    ctx->LocalToString(params[1], &cvarName);
+    ctx->LocalToString(params[2], &destBuffer);
+
+    const char *value = CVAR_GET_STRING(cvarName);
+
+    std::strncpy(destBuffer, value, bufferSize);
+
+    destBuffer[bufferSize - 1] = '\0';
+
+    return 1;
+}
+
+// native int cvarGetInt(const char[] name)
+static cell_t core_cvarGetInt(SourcePawn::IPluginContext *ctx,
+                              const cell_t *params)
+{
+    char *cvarName;
+    ctx->LocalToString(params[1], &cvarName);
+
+    return CVAR_GET_FLOAT(cvarName);
+}
+
+// native int cvarGetFlags(const char[] name)
+static cell_t core_cvarGetFlags(SourcePawn::IPluginContext *ctx,
+                                const cell_t *params)
+{
+    char *cvarName;
+    ctx->LocalToString(params[1], &cvarName);
+
+    cvar_t *cvar = CVAR_GET_POINTER(cvarName);
+    if (!cvar)
+        return 0;
+
+    return cvar->flags;
+}
+
+// native void cvarSetFloat(const char[] name, float value)
+static cell_t core_cvarSetFloat(SourcePawn::IPluginContext *ctx,
+                                const cell_t *params)
+{
+    char *cvarName;
+    ctx->LocalToString(params[1], &cvarName);
+
+    CVAR_SET_FLOAT(cvarName, sp_ctof(params[2]));
+
+    return 1;
+}
+
+// native void cvarSetString(const char[] name, const char[] value)
+static cell_t core_cvarSetString(SourcePawn::IPluginContext *ctx,
+                                 const cell_t *params)
+{
+    char *cvarName, *cvarValue;
+    ctx->LocalToString(params[1], &cvarName);
+    ctx->LocalToString(params[2], &cvarValue);
+
+    CVAR_SET_STRING(cvarName, cvarValue);
+
+    return 1;
+}
+
+// native void cvarSetInt(const char[] name, int value)
+static cell_t core_cvarSetInt(SourcePawn::IPluginContext *ctx,
+                              const cell_t *params)
+{
+    char *cvarName;
+    ctx->LocalToString(params[1], &cvarName);
+
+    CVAR_SET_FLOAT(cvarName, params[2]);
+
+    return 1;
+}
+
+// native void cvarSetFlags(const char[] name, int flags)
+static cell_t core_cvarSetFlags(SourcePawn::IPluginContext *ctx,
+                                const cell_t *params)
+{
+    char *cvarName;
+    ctx->LocalToString(params[1], &cvarName);
+
+    cvar_t *cvar = CVAR_GET_POINTER(cvarName);
+    if (!cvar)
+        return 0;
+
+    cvar->flags = params[2];
+
+    return 1;
+}
+
+// native void cvarRegister(const char[] name, const char[] value, int flags = 0)
+static cell_t core_cvarRegister(SourcePawn::IPluginContext *ctx,
+                                const cell_t *params)
+{
+    char *cvarName, *cvarValue;
+    ctx->LocalToString(params[1], &cvarName);
+    ctx->LocalToString(params[2], &cvarValue);
+
+    cvar_t cvarToRegister;
+    cvarToRegister.name = cvarName;
+    cvarToRegister.string = cvarValue;
+    cvarToRegister.flags = params[3];
+
+    CVAR_REGISTER(&cvarToRegister);
+
+    return 1;
+}
+
 sp_nativeinfo_t gCoreNatives[] =
 {
     { "printToConsole",     core_printToConsole     },
@@ -314,5 +438,14 @@ sp_nativeinfo_t gCoreNatives[] =
     { "executeForward",     core_executeForward     },
     { "prepareFwdArray",    core_prepareFwdArray    },
     { "prepareFwdString",   core_prepareFwdString   },
+    { "cvarGetFloat",       core_cvarGetFloat       },
+    { "cvarGetString",      core_cvarGetString      },
+    { "cvarGetInt",         core_cvarGetInt         },
+    { "cvarGetFlags",       core_cvarGetFlags       },
+    { "cvarSetFloat",       core_cvarSetFloat       },
+    { "cvarSetString",      core_cvarSetString      },
+    { "cvarSetInt",         core_cvarSetInt         },
+    { "cvarSetFlags",       core_cvarSetFlags       },
+    { "cvarRegister",       core_cvarRegister       },
     { nullptr,              nullptr                 }
 };
