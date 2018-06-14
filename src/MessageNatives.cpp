ed98dc57fc6dbd401b24272a8e284fda54492415
Mistrick <mistrick94@gmail.com>
2018-06-14 12:21:11 +0700

    fixes for complilation

--- src/MessageNatives.cpp
+++ src/MessageNatives.cpp
@@ -18,22 +18,24 @@
 #include "spmod.hpp"
 
 // int GetUserMsgId(const char[] msgname);
-static cell_t GetUserMsgId(SourcePawn::IPluginContext *ctx, const cell_t *params) /* 1 param */
+static cell_t GetUserMsgId(SourcePawn::IPluginContext *ctx,
+                           const cell_t *params)
 {
     enum { arg_msgname = 1 };
     
     char *string;
     ctx->LocalToString(params[arg_msgname], &string);
 
-    return GET_USER_MSG_ID(PLID, string, NULL);
+    return GET_USER_MSG_ID(PLID, string, nullptr);
 }
 
 // void GetUserMsgName(int msgid, char[] str, int len) = 3 params
-static cell_t GetUserMsgName(SourcePawn::IPluginContext *ctx, const cell_t *params)
+static cell_t GetUserMsgName(SourcePawn::IPluginContext *ctx,
+                             const cell_t *params)
 {
     enum { arg_msg = 1, arg_str, arg_len };
 
-    const char* string = GET_USER_MSG_NAME(PLID, params[arg_msg], NULL);
+    const char* string = GET_USER_MSG_NAME(PLID, params[arg_msg], nullptr);
     if (string)
     {
         ctx->StringToLocal(params[arg_str], params[arg_len], string);
@@ -43,18 +45,19 @@ static cell_t GetUserMsgName(SourcePawn::IPluginContext *ctx, const cell_t *para
     return 0;
 }
 
-static cell_t MessageBegin_(SourcePawn::IPluginContext *ctx, const cell_t *params, bool useFloat) /* 4 param */
+static cell_t MessageBegin_(SourcePawn::IPluginContext *ctx,
+                            const cell_t *params, bool useFloat)
 {
     enum { arg_dest = 1, arg_msg_type, arg_origin, arg_player };
 
-    int numparam = *params / sizeof(cell_t);
+    int numparam = *params;
     float vecOrigin[3];
     cell_t *cpOrigin;
 
     if (params[arg_msg_type] < 1 || ((params[arg_msg_type] > 63)		// maximal number of engine messages
-        && !GET_USER_MSG_NAME(PLID, params[2], NULL)))
+        && !GET_USER_MSG_NAME(PLID, params[2], nullptr)))
     {
-        // LogError(amx, AMX_ERR_NATIVE, "Plugin called message_begin with an invalid message id (%d).", params[2]);
+        ctx->ReportError("Plugin called message_begin with an invalid message id (%d).", params[arg_msg_type]);
         return 0;
     }
 
@@ -64,13 +67,13 @@ static cell_t MessageBegin_(SourcePawn::IPluginContext *ctx, const cell_t *param
     case MSG_ALL:
     case MSG_SPEC:
     case MSG_INIT:
-        MESSAGE_BEGIN(params[arg_dest], params[arg_msg_type], NULL);
+        MESSAGE_BEGIN(params[arg_dest], params[arg_msg_type], nullptr);
         break;
     case MSG_PVS: case MSG_PAS:
     case MSG_PVS_R: case MSG_PAS_R:
         if (numparam < 3)
         {
-            // LogError(amx, AMX_ERR_NATIVE, "Invalid number of parameters passed");
+            ctx->ReportError("Invalid number of parameters passed");
             return 0;
         }
 
@@ -96,11 +99,11 @@ static cell_t MessageBegin_(SourcePawn::IPluginContext *ctx, const cell_t *param
     case MSG_ONE:
         if (numparam < 4)
         {
-            // LogError(amx, AMX_ERR_NATIVE, "Invalid number of parameters passed");
+            ctx->ReportError("Invalid number of parameters passed");
             return 0;
         }
         
-        MESSAGE_BEGIN(params[arg_dest], params[arg_msg_type], NULL, INDEXENT(params[arg_player]));
+        MESSAGE_BEGIN(params[arg_dest], params[arg_msg_type], nullptr, INDEXENT(params[arg_player]));
         break;
     }
 
@@ -108,91 +111,102 @@ static cell_t MessageBegin_(SourcePawn::IPluginContext *ctx, const cell_t *param
 }
 
 // native void MessageBegin(int dest, int msgid, const int origin[3] = {0,0,0}, int player = 0);
-static cell_t MessageBegin(SourcePawn::IPluginContext *ctx, const cell_t *params)
+static cell_t MessageBegin(SourcePawn::IPluginContext *ctx,
+                           const cell_t *params)
 {
-    MessageBegin_(ctx, params, false);
-    return 1;
+    return MessageBegin_(ctx, params, false);
 }
 
 // native void MessageBeginF(int dest, int msgid, const float origin[3] = {0.0,0.0,0.0}, int player = 0);
-static cell_t MessageBeginF(SourcePawn::IPluginContext *ctx, const cell_t *params)
+static cell_t MessageBeginF(SourcePawn::IPluginContext *ctx,
+                            const cell_t *params)
 {
-    MessageBegin_(ctx, params, true);
-    return 1;
+    return MessageBegin_(ctx, params, true);
 }
 
 // native void MessageEnd();
-static cell_t MessageEnd(SourcePawn::IPluginContext *ctx, const cell_t *params)
+static cell_t MessageEnd(SourcePawn::IPluginContext *ctx [[maybe_unused]],
+                         const cell_t *params [[maybe_unused]])
 {
     MESSAGE_END();
     return 1;
 }
 
 // native void WriteByte(int value);
-static cell_t WriteByte(SourcePawn::IPluginContext *ctx, const cell_t *params)
+static cell_t WriteByte(SourcePawn::IPluginContext *ctx [[maybe_unused]],
+                        const cell_t *params)
 {
     enum { arg_value = 1 };
     WRITE_BYTE(params[arg_value]);
     return 1;
 }
 // native void WriteChar(char value);
-static cell_t WriteChar(SourcePawn::IPluginContext *ctx, const cell_t *params)
+static cell_t WriteChar(SourcePawn::IPluginContext *ctx [[maybe_unused]],
+                        const cell_t *params)
 {
     enum { arg_value = 1 };
     WRITE_CHAR(params[arg_value]);
     return 1;
 }
 // native void WriteShort(int value);
-static cell_t WriteShort(SourcePawn::IPluginContext *ctx, const cell_t *params)
+static cell_t WriteShort(SourcePawn::IPluginContext *ctx [[maybe_unused]],
+                        const cell_t *params)
 {
     enum { arg_value = 1 };
     WRITE_SHORT(params[arg_value]);
     return 1;
 }
 // native void WriteLong(int value);
-static cell_t WriteLong(SourcePawn::IPluginContext *ctx, const cell_t *params)
+static cell_t WriteLong(SourcePawn::IPluginContext *ctx [[maybe_unused]],
+                        const cell_t *params)
 {
     enum { arg_value = 1 };
     WRITE_LONG(params[arg_value]);
     return 1;
 }
 // native void WriteEntity(int value);
-static cell_t WriteEntity(SourcePawn::IPluginContext *ctx, const cell_t *params)
+static cell_t WriteEntity(SourcePawn::IPluginContext *ctx [[maybe_unused]],
+                          const cell_t *params)
 {
     enum { arg_value = 1 };
     WRITE_ENTITY(params[arg_value]);
     return 1;
 }
 // native void WriteAngle(int value);
-static cell_t WriteAngle(SourcePawn::IPluginContext *ctx, const cell_t *params)
+static cell_t WriteAngle(SourcePawn::IPluginContext *ctx [[maybe_unused]],
+                         const cell_t *params)
 {
     enum { arg_value = 1 };
     WRITE_ANGLE(static_cast<float>(params[arg_value]));
     return 1;
 }
 // native void WriteAngleF(float value);
-static cell_t WriteAngleF(SourcePawn::IPluginContext *ctx, const cell_t *params)
+static cell_t WriteAngleF(SourcePawn::IPluginContext *ctx [[maybe_unused]],
+                          const cell_t *params)
 {
     enum { arg_value = 1 };
     WRITE_ANGLE(sp_ctof(params[arg_value]));
     return 1;
 }
 // native void WriteCoord(int value);
-static cell_t WriteCoord(SourcePawn::IPluginContext *ctx, const cell_t *params)
+static cell_t WriteCoord(SourcePawn::IPluginContext *ctx [[maybe_unused]],
+                         const cell_t *params)
 {
     enum { arg_value = 1 };
     WRITE_COORD(static_cast<float>(params[arg_value]));
     return 1;
 }
 // native void WriteCoordF(float value);
-static cell_t WriteCoordF(SourcePawn::IPluginContext *ctx, const cell_t *params)
+static cell_t WriteCoordF(SourcePawn::IPluginContext *ctx [[maybe_unused]],
+                          const cell_t *params)
 {
     enum { arg_value = 1 };
     WRITE_COORD(sp_ctof(params[arg_value]));
     return 1;
 }
 // native void WriteString(char[] value);
-static cell_t WriteString(SourcePawn::IPluginContext *ctx, const cell_t *params)
+static cell_t WriteString(SourcePawn::IPluginContext *ctx,
+                          const cell_t *params)
 {
     enum { arg_value = 1 };
     char *string;
