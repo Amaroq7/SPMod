608bdd4243eb0b8f65ff7a0d8d0840ef8e35599d
Mistrick <mistrick94@gmail.com>
2018-07-31 08:57:06 +0700

    Missed code

--- src/dllapi.cpp
+++ src/dllapi.cpp
@@ -59,11 +59,12 @@ static void ClientCommand(edict_t *pEntity)
     }
 
     META_RES res = MRES_IGNORED;
+
+    std::string strCmd(CMD_ARGV(0));
+
     const std::unique_ptr<CommandMngr> &cmdMngr = gSPGlobal->getCommandManagerCore();
     if (cmdMngr->getCommandsNum(CmdType::Client))
     {
-        std::string strCmd(CMD_ARGV(0));
-
         if (!strCmd.compare("say") || !strCmd.compare("say_team"))
         {
             strCmd += ' ';
@@ -89,6 +90,11 @@ static void ClientCommand(edict_t *pEntity)
                 }
             }
         }
+    }
+
+    if (!strCmd.compare("menuselect"))
+    {
+        res = gSPGlobal->getMenuManagerCore()->ClientCommand(pEntity);
     }
 
     RETURN_META(res);
