657b405cac23b785bdf8b0cc1b4b7b46c03f7c1e
Dominik Szamburski <dominikszamburski99@gmail.com>
2019-10-02 18:43:07 +0200

    fix

--- appveyor/package.ps1
+++ appveyor/package.ps1
@@ -10,10 +10,10 @@ $CC_VERSION = $env:APPVEYOR_BUILD_WORKER_IMAGE.Substring($env:APPVEYOR_BUILD_WOR
 
 if ($env:APPVEYOR_REPO_TAG_NAME -eq $null) {
     $ARCHIVE_NAME = "spmod-win32-$COMMIT_NUM-$COMMIT_SHORT_SHA-msvc$CC_VERSION-$env:LINK_TYPE.7z"
-    7z a -t7z -mm=LZMA:d256m:fb64 -mx9 "$ARCHIVE_NAME" dlls scripts
+    7z a -t7z -mm=LZMA:d256m:fb64 -mx9 "$ARCHIVE_NAME" dlls scripting
     Push-AppveyorArtifact $ARCHIVE_NAME
 } else {
     $ARCHIVE_NAME = "spmod-win32-$env:APPVEYOR_REPO_TAG_NAME-msvc$CC_VERSION-$env:LINK_TYPE.7z"
-    7z a -t7z -mm=LZMA:d256m:fb64 -mx9 "$ARCHIVE_NAME" dlls scripts
+    7z a -t7z -mm=LZMA:d256m:fb64 -mx9 "$ARCHIVE_NAME" dlls scripting
     Push-AppveyorArtifact $ARCHIVE_NAME
 }
