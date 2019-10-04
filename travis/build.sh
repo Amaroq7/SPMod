0fea78aaf5b4c537f7cc3f62d66d3b2dc27cbf20
Karol Szuster <karolsz9898@gmail.com>
2019-10-05 00:18:43 +0200

    Refresh build system
    Signed-off-by: Karol Szuster <karolsz9898@gmail.com>

--- travis/build.sh
+++ travis/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir -p build
 cd build
-python /usr/bin/meson .. . --buildtype release --warnlevel 3 -D linktype=$LINK_TYPE -D b_lto=true
+python /usr/bin/meson .. . --buildtype release --werror --optimization 3 --warnlevel 3 -D linktype=$LINK_TYPE -D b_lto=true -D cpp_std=c++17 -D c_std=c11
 python /usr/bin/meson configure
 ninja -v
