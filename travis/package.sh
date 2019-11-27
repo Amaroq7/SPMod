20534e3248dfd61bcd35150787eb6a5158e8e914
Karol Szuster <karolsz9898@gmail.com>
2019-11-27 20:38:56 +0100

    Some code fixes reported by code-inspector
    Signed-off-by: Karol Szuster <karolsz9898@gmail.com>

--- travis/package.sh
+++ travis/package.sh
@@ -4,32 +4,32 @@
 XZ_OPT="-9e --threads=0"
 
 # Make folders
-mkdir -p $TRAVIS_BUILD_DIR/upload/dlls
-mkdir -p $TRAVIS_BUILD_DIR/upload/scripting
+mkdir -p "$TRAVIS_BUILD_DIR/upload/dlls"
+mkdir -p "$TRAVIS_BUILD_DIR/upload/scripting"
 
 # Move SPMod lib to dlls
-mv $TRAVIS_BUILD_DIR/build/src/libspmod_mm.so $TRAVIS_BUILD_DIR/upload/dlls
+mv "$TRAVIS_BUILD_DIR/build/src/libspmod_mm.so" "$TRAVIS_BUILD_DIR/upload/dlls"
 
 if [[ $TRAVIS_TAG ]]
 then
-    ARCHIVE_NAME=spmod-$TRAVIS_OS_NAME-$TRAVIS_TAG-$CC$CC_VERSION-$LINK_TYPE.tar.xz
+    ARCHIVE_NAME="spmod-$TRAVIS_OS_NAME-$TRAVIS_TAG-$CC$CC_VERSION-$LINK_TYPE.tar.xz"
 else
-    ARCHIVE_NAME=spmod-$TRAVIS_OS_NAME-$COMMIT_NUM-$COMMIT_SHORT_SHA-$CC$CC_VERSION-$LINK_TYPE.tar.xz
+    ARCHIVE_NAME="spmod-$TRAVIS_OS_NAME-$COMMIT_NUM-$COMMIT_SHORT_SHA-$CC$CC_VERSION-$LINK_TYPE.tar.xz"
 fi
 
-cp -r $TRAVIS_BUILD_DIR/scripting/* $TRAVIS_BUILD_DIR/upload/scripting
+cp -r "$TRAVIS_BUILD_DIR/scripting/*" "$TRAVIS_BUILD_DIR/upload/scripting"
 
-cd $TRAVIS_BUILD_DIR/upload
+cd "$TRAVIS_BUILD_DIR/upload" || exit
 if [[ $LINK_TYPE == "dynamic" && ! -z $CLANG_VERSION ]]
 then
     mkdir -p libs
-    mv $TRAVIS_BUILD_DIR/build/libc++abi.so.1 libs
-    mv $TRAVIS_BUILD_DIR/build/libc++.so.1 libs
+    mv "$TRAVIS_BUILD_DIR/build/libc++abi.so.1" libs
+    mv "$TRAVIS_BUILD_DIR/build/libc++.so.1" libs
 
-    tar -cJvf $ARCHIVE_NAME dlls scripting libs
+    tar -cJvf "$ARCHIVE_NAME" dlls scripting libs
 else
-    tar -cJvf $ARCHIVE_NAME dlls scripting
+    tar -cJvf "$ARCHIVE_NAME" dlls scripting
 fi
 
 mkdir -p s3
-cp $ARCHIVE_NAME s3
+cp "$ARCHIVE_NAME" s3
